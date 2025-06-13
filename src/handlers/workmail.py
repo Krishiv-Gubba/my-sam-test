12a183228b616aa2d7cb75b8d398769960c09fa2
Krishiv Gubba <krishiv@irisfinance.co>
2025-06-14 02:58:32 +0530

    something

---
+++ src/handlers/workmail.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
+import boto3
+import botocore
+import openai
+from pydantic import BaseModel
+
+# pip install boto3>=1.26.0 botocore==1.38.29 numpy==1.26.4 pandas==2.3.0 pydantic==2.11.5 openai==1.86.0 python-dotenv==1.1.0
+
+# load_dotenv()
+
+def lambda_handler(event, context):
+    return {"message": "Test Lambda ran successfully"}
