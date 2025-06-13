ad0a50afe3ec8579606e7b20a996db203d253d25
Krishiv Gubba <krishiv@irisfinance.co>
2025-06-14 03:24:32 +0530

    buck4

--- src/handlers/workmail.py
+++ src/handlers/workmail.py
@@ -6,7 +6,7 @@ import botocore
 import openai
 from pydantic import BaseModel
 
-# pip install boto3>=1.26.0 botocore==1.38.29 numpy==1.26.4 pandas==2.3.0 pydantic==2.11.5 openai==1.86.0 python-dotenv==1.1.0
+# pip install boto3>=1.26.0 botocore==1.38.29 numpy==1.26.4 pandas==2.3.0 pydantic==2.11.5 openai==1.86.0 python-dotenv==1.1.01
 
 # load_dotenv()
 
